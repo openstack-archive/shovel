091e0a4535ac5aa72cb0b881e83263c069747f98
keedya <andre.keedy@emc.com>
2015-12-10 11:00:50 -0500

    Update installl.sh

--- Horizon/install.sh
+++ Horizon/install.sh
@@ -5,28 +5,28 @@ TEMP=`getopt -o u:l: --long url: --long location: -- "$@"`
 if [ $? != 0 ] ; then echo "Exit" ; exit 1 ; fi
 
 eval set -- "$TEMP"
-echo $TEMP
-ADDR_IP=${ADDR_IP-}
+SHOVEL_URL=${SHOVEL_URL-}
 FILE_LOC=${FILE_LOC-}
 
 while true ; do
         case "$1" in
-		-u | --url) ADDR_IP=$2 ;shift 2 ;;
+		-u | --url) SHOVEL_URL=$2 ;shift 2 ;;
 		-l | --location) FILE_LOC=$2;shift 2 ;; 
 		--) shift; break ;;
 		*) echo "Internal error!" ; exit 1 ;;
         esac 
 done 
-echo "get shovel url: " $ADDR_IP 
+echo "get shovel url: " $SHOVEL_URL 
 echo "get file location: " $FILE_LOC 
-if [ -z "$ADDR_IP" -o -z "$FILE_LOC" ] 
+if [ -z "$SHOVEL_URL" -o -z "$FILE_LOC" ] 
 then
-   echo "You must specify ipaddr of shovel and horizon location"
+   echo "You must specify Shovel service URL(http://<ipaddr>)using --url <shovel-url>"
+   echo "and horizon location using --location <horizon path>"
    exit 1
 fi
 
-#replace in shovel.py  SHOVEL_URL with the new addre value
-sed -i "s|.*URI = .*|URI = \"$ADDR_IP\" + SHOVEL_BASE_API|g" rackhd/shovel.py
+#replace in shovel.py  SHOVEL_URL with the new url value
+sed -i "s|.*URI = .*|URI = \"$SHOVEL_URL\" + SHOVEL_BASE_API|g" rackhd/shovel.py
 #copy rackhd to horizon admin dashboard
 cp -r rackhd $FILE_LOC/openstack_dashboard/dashboards/admin
 #copy _50_admin_rackhd_panels.py to dashboard enabled
